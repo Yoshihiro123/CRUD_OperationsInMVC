67c1f0d6bc9c43ac1efc3eb9df5e47aacec06cee
Yoshihiro123 <yoshihirotakaki735@gmail.com>
2022-04-09 20:44:43 +0900

    initial commit

---
+++ CRUD_OperationsInMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using CRUD_OperationsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsInMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            List<Department> listDepartments = dbContext.Department.ToList();
+            return View(listDepartments);
+        }
+    }
+}
---
+++ CRUD_OperationsInMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using CRUD_OperationsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            List<Employee> employees = dbContext.Employee.Where(emp => emp.EmployeeId == departmentId).ToList();
+            return View(employees);
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            Employee employee = dbContext.Employee.FirstOrDefault(x => x.EmployeeId == id);
+            return View(employee);
+        }
+    }
+}
